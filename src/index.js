f19d5b48163a93f7fbb32fab1b974c847271612f
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-15 12:31:40 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let newForm = document.getElementById("create-task-form")
+  newForm.addEventListener("submit", addToDO)
+  let newList = document.getElementById("tasks")
+  newList.addEventListener('click', deleteToDo)
+})
+
+
+
+function addToDO(event) {
+  event.preventDefault()
+
+  let newDescription = document.getElementById("new-task-description")
+  let value = newDescription.value
+  let newList = document.getElementById("tasks")
+  let newLi = document.createElement('li')
+  newLi.innerText = value
+  newList.appendChild(newLi)
+
+
+  let button = document.createElement("button")
+  button.innerText = "d"
+  newLi.append(button)
+  newDescription.value = 'element created'
+
+
+}
+
+
+function deleteToDo(event) {
+  console.log(event)
+  event.target.parentElement.remove()
+}
